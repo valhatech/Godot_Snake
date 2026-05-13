fd3245075c5541a3bafb6997bb979e4dcf183e8d
bnf <fred.beaupin@gmail.com>
2026-05-13 23:51:22 +0200

    add new rs file

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,10 @@
+use godot::prelude::*;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ rust/src/player.rs
@@ -0,0 +1,11 @@
+use godot::prelude::*;
+use godot::classes::Sprite2D;
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+
+    base: Base<Sprite2D>
+}
